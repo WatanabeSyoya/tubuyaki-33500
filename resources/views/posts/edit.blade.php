60d32b35c9af41713bfbf69a536fda42819ce4b1
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-03-03 22:51:08 +0900

    編集画面

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -25,13 +25,13 @@
                 @method('PUT')
               <div class="form-group">
                 <label for="exampleInputEmail1">title</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="title" name="title">
+                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="title" name="title" value="{{ $post->title }}">
               </div>
 
 
                <div class="form-group">
                     <label for="exampleFormControlSelect1">category</label>
-                    <select class="form-control" id="exampleFormControlSelect1" name="category_id">
+                    <select class="form-control" id="exampleFormControlSelect1" name="category_id" value="{{ $post->category->category_id }}">
                         <option selected="">選択する</option>
                         <option value="1">book</option>
                         <option value="2">cafe</option>
@@ -41,7 +41,7 @@
 
                 <div class="form-group">
                   <label for="comment">Comment:</label>
-                  <textarea class="form-control" rows="5" id="comment" name="content"></textarea>
+                  <textarea class="form-control" rows="5" id="comment" name="content" value="{{ $post->comment }}"></textarea>
                 </div>
 
                 <input type="hidden" name="user_id" value="{{ Auth::id() }}">
