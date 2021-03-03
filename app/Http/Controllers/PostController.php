64cb61a0096cbd6883887b4e3f588d6215942c80
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-03-03 21:49:51 +0900

    編集画面に遷移

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -87,7 +87,7 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        $post = Todo::find($id);
+        $post = Post::find($id);
 
         return view('posts.edit', compact('post'));
     }
