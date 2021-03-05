793ba3fd97b9608fd3162ca218184dc906337a02
WatanabeSyoya <76427150+WatanabeSyoya@users.noreply.github.com>
2021-03-05 19:09:11 +0900

    認証

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -87,10 +87,15 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        
-        $post = Post::find($id);
+        if( Auth::id() !== $post->user->id ){
+            return redirect('/');
 
-        return view('posts.edit', compact('post'));
+        } else {
+            $post = Post::find($id);
+
+            return view('posts.edit', compact('post'));
+        }
+
     }
 
     /**
